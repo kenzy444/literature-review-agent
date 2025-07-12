2642725e67cd2d5e4920f8cf288fec615f42ad54
kenzy444 <khelkhalkenza88@gmail.com>
2025-07-13 00:25:34 +0200

    before changing

--- test_config.py
+++
@@ -1,6 +0,0 @@
-from src.core.exceptions import ExternalServiceError
-
-try:
-    raise ExternalServiceError("HuggingFace", "timeout")
-except ExternalServiceError as e:
-    print("Caught error:", str(e))
